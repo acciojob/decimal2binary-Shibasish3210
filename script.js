4496d698993333e55aa35a59436205abceea6a46
Shibasish3210 <111530472+Shibasish3210@users.noreply.github.com>
2023-08-07 17:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let str = '';
+	while (num>0) {
+		str += num % 2;
+		num = Math.floor(num/2);
+	}
+  alert(str.split('').reverse().join(''));
 }
+decimalToBinary(10);
 
 window.decimalToBinary = decimalToBinary;
